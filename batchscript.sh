21eeab115814904c045f5ac6bd95c89887f1dee1
Sven Lundgren <lundgren87@gmail.com>
2019-11-19 16:37:56 +0100

    Just a better batchscript.

--- batchscript.sh
+++ batchscript.sh
@@ -1,9 +1,13 @@
 #! /bin/bash -l
 #SBATCH -J paratest
 #SBATCH -p cluster
-#SBATCH -t 5:00
-#SBATCH -o output.out
-#SBATCH -N 4
+#SBATCH -t 15:00
+#SBATCH -o results/para_8N
+#SBATCH -N 8
+
+NNODES=8
+ITERATIONS=500
+ARRAYSIZE=67108864
 
 echo SETTING UP ENVIRONMENT
 cd ${HOME}/git/paratest/
@@ -24,5 +28,17 @@ echo === RUNNING PARATEST ===
 mpirun  --map-by ppr:1:node         \
         --mca mpi_leave_pinned 1    \
         --mca osc pt2pt             \
-        -n 4 ./paratest
+        -n ${NNODES} ./paratest -s ${ARRAYSIZE} -i ${ITERATIONS} -n 1
+mpirun  --map-by ppr:1:node         \
+        --mca mpi_leave_pinned 1    \
+        --mca osc pt2pt             \
+        -n ${NNODES} ./paratest -s ${ARRAYSIZE} -i ${ITERATIONS} -n 2
+mpirun  --map-by ppr:1:node         \
+        --mca mpi_leave_pinned 1    \
+        --mca osc pt2pt             \
+        -n ${NNODES} ./paratest -s ${ARRAYSIZE} -i ${ITERATIONS} -n 4
+mpirun  --map-by ppr:1:node         \
+        --mca mpi_leave_pinned 1    \
+        --mca osc pt2pt             \
+        -n ${NNODES} ./paratest -s ${ARRAYSIZE} -i ${ITERATIONS} -n 8
 echo === PARATEST FINISHED ===
